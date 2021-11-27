c7448d64b5a34c10b984cdeaacfa42138f94d1fa
superbright <>
2021-11-27 17:45:06 +0000

    Batch Process 2080x2080 only

--- DOSPunkText.py
+++ DOSPunkText.py
@@ -34,8 +34,10 @@ def hash_distance(hash1, hash2):
 # Compute the average hash of the given image.
 def average_hash(image, hash_size=80):
     
-    # resize image and convert it to grayscal.
-    image = image.resize((hash_size, hash_size), Image.ANTIALIAS)
+    # resize image and convert it to grayscale.
+    image = ImageOps.autocontrast(image)
+    if (not optimize):
+        image = image.resize((hash_size, hash_size), Image.ANTIALIAS)
     image = ImageOps.grayscale(image)
     
     pixels = list(image.getdata())
@@ -43,7 +45,11 @@ def average_hash(image, hash_size=80):
 
     # Compute the hash based on each pixels value compared to the average.
     bits = "".join(map(lambda pixel: '1' if pixel > avg else '0', pixels))
-    hashformat = "0{hashlength}x".format(hashlength=hash_size ** 2 // 4)
+    if optimize:
+        hashformat = "0{hashlength}x".format(hashlength=(80 * 160) // 4)
+    else:    
+        hashformat = "0{hashlength}x".format(hashlength=hash_size ** 2 // 4)
+    
     return int(bits, 2).__format__(hashformat)
 
 # Compute the hamming distance between two images
@@ -90,6 +96,12 @@ def create_font_record(image):
 # create an image for each character in the font and store
 # the average hash of the image against the unicode 
 def create_font_blocks():
+
+    # add the space character 
+    image = create_char_image(0x00A0)
+    image = image.convert("L")
+    font_blocks[0x00A0] = create_font_record(image)
+
     # create images of non-blank characters from the font
     for char_code in range(0x0021, 0x0266C):
       image = create_char_image(char_code)
@@ -99,10 +111,6 @@ def create_font_blocks():
           font_blocks[char_code] = create_font_record(image)
           if debug:
               image.save(f"{dir_font_blocks}/{char_code}.png")
-    # add the space character back in
-    image = create_char_image(0x00A0)
-    image = image.convert("L")
-    font_blocks[0x00A0] = create_font_record(image)
         
 # split the DOS punk image up into chracter blocks
 # width 80, height 160 in 
@@ -112,28 +120,9 @@ def create_punk_blocks(filename):
 
     image = Image.open(filename).convert('RGB')
 
-    if ((image.width != 208) or (image.height != 208)) and ((image.width != 1280) or (image.height != 1280)):
-        image = image.resize( (208, 208), Image.LANCZOS)
-        errors.append(f"{filename} not 208x208 pixels")
-
-    # check image is square
-    if (image.width != image.height):
-        image = image.resize( (208, 208), Image.LANCZOS)
-        errors.append(f"{filename} does not have equal width & height")
-        
-
-    # resize to 1280x1280 if required
-    if (image.width !=1280):
-        if optimize:
-            # optimize for character matching
-            image = image.resize( (1280, 1280), Image.LANCZOS)
-        else:
-            # optimize for color matching
-            image = image.resize( (1280, 1280), Image.NONE)
-        enhancer = ImageEnhance.Sharpness(image)
-        image = enhancer.enhance(4)
-        if debug:
-            image.save(f"{dir_punk_blocks}/image.png")
+    if (image.width != 1280) or (image.height != 1280):
+        errors.append(f"{filename} not 1280x1280 pixels")
+        print(f"{filename} not 1280x1280 pixels")    
 
     # split image up into blocks
     block_num = 1
@@ -204,6 +193,11 @@ def match_blocks():
     check = False
     block_errors.clear()
 
+    if optimize:
+        error_threshold = 0
+    else:
+        error_threshold = 20
+
     for num, image in enumerate(image_blocks, start=1):
     
         # output a new console line every 16 blocks
@@ -217,26 +211,36 @@ def match_blocks():
         img_hash = average_hash(image)
         img_hash_inv = average_hash(ImageOps.invert(image))
 
+        # get the number of colours in the block
+        clrs = len(image.getcolors())
+        is_space = (clrs == 1)
+
         # setup some default values for the match
         best_score = sys.maxsize
         char_code = 0
         inverted = False
         
-        # match the block or it's inverse to a character in the font
-        for key in font_blocks:
-            char_hash = font_blocks[key].hash
-            # match against font block
-            distance = hash_distance(char_hash, img_hash)
-            if distance < best_score:
-                best_score = distance
-                char_code = key
-                inverted = False
-            # match against reversed font block    
-            distance = hash_distance(char_hash, img_hash_inv)
-            if distance < best_score:
-                best_score = distance
-                char_code = key
-                inverted = True
+        if is_space:
+            # space character
+            char_code = 0x00A0
+            best_score = 0
+            inverted = False
+        else:
+            # match the block or it's inverse to a character in the font
+            for key in font_blocks:
+                char_hash = font_blocks[key].hash
+                # match against font block
+                distance = hash_distance(char_hash, img_hash)
+                if distance < best_score:
+                    best_score = distance
+                    char_code = key
+                    inverted = False
+                # match against reversed font block    
+                distance = hash_distance(char_hash, img_hash_inv)
+                if distance < best_score:
+                    best_score = distance
+                    char_code = key
+                    inverted = True
   
         text += chr(char_code)
         metadata["text"]+= chr(char_code)
@@ -250,13 +254,16 @@ def match_blocks():
         char_freq[char_code]+=1
 
         if (char_code != 0x00A0) and (fg == bg):
+            block_errors.append(f"mutant block {num}: Score: {best_score} x:{num % 16} y:{(num // 16)+1}")
             mutant = True
 
-        if (char_code == 0x00A0) and (best_score > 300):
+        if (best_score > error_threshold):
             check = True
+            block_errors.append(f"check block {num}: Score: {best_score} x:{num % 16} y:{(num // 16)+1}")
 
-        #if best_score > 20:
-        #    block_errors.append(f"check block {num}: x:{num % 16} y:{(num // 16)+1}")
+        if (char_code == 0x00A0) and (not is_space):
+            check = True
+            block_errors.append(f"Block:{num} is not a space!")
 
         print_color(chr(char_code), fg, bg)
         
